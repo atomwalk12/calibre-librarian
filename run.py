ed11581888db779c99c055c529d4694be80a22d5
Razvan <atomwalk12@gmail.com>
2024-06-13 21:36:50 +0200

    introduce Librarian dependency

--- run.py
+++ run.py
@@ -1,22 +1,20 @@
 import gradio as gr
 import argparse
-
-from librarian.librarian import llm_client, system_prompt, chat_history
-
+from agent.librarian import Librarian
 
 # Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--name", type=str, default="User")
 args, unknown = parser.parse_known_args()
 
-
+agent = Librarian("./books")
 # Gradio
-def predict(user_input, test):
+def predict(user_input, _):
     global chat_history
 
     # If it's the start of the conversation, include the system prompt
     if chat_history is None or len(chat_history) == 0:
-        chat_history = [{"role": "system", "content": system_prompt}]
+        chat_history = [{"role": "system", "content": agent.system_prompt}]
     else:
         chat_history = chat_history.copy()  # Make a copy to avoid modifying the input directly
 
@@ -25,7 +23,7 @@ def predict(user_input, test):
 
     # Display librarian's answer
     full_message = ""
-    for token in llm_client.chat_completion(chat_history, max_tokens=100, stream=True):
+    for token in agent.llm_client.chat_completion(chat_history, max_tokens=100, stream=True):
         full_message += token.choices[0].delta.content
 
         yield full_message
