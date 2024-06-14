5af5b82e2afb6094d0f3187850ed376fb34093e7
Razvan <atomwalk12@gmail.com>
2024-06-14 15:19:31 +0200

    chore: refactoring step

--- agent/librarian.py
+++ agent/librarian.py
@@ -1,12 +1,10 @@
 from huggingface_hub import InferenceClient
 from llama_index.core import SimpleDirectoryReader
 import json
+import logging
 
 
-class Librarian():
-    # Model to use
-    repo_id = "meta-llama/Meta-Llama-3-8B-Instruct"
-
+class Librarian:
     # Track chat history
     chat_history = []
 
@@ -16,18 +14,45 @@ class Librarian():
         Please provide insightful, concise and helpful information about the books in response to my questions.
     """
 
+    def __init__(self, name, lib_path, model_id):
+        """
+        Initializes the Librarian, which represents a wrapper around the LLM.
 
-    def __init__(self, lib_path):
+        :param name: User's name.
+        :param lib_path: The path where the books are stored.
+        :param model_id: The Huggingface model id.
+        """
+        # User's name
+        self.name = name
+
         # Load all books from the library
         self.books = self.load_books(lib_path)
 
+        # Logger
+        self.logger = logging.getLogger("librarian_logger")
+
         # Inference client definition
         self.llm_client = InferenceClient(
-            model=self.repo_id,
+            model=model_id,
             timeout=120,
         )
 
-    def call_llm(self, inference_client: InferenceClient, prompt: dict):
+    def stream_response(self):
+        """
+        The response from the LLM is streamed to the Gradio model via the yield keyword.
+        """
+        logging.info(f"Chat history: {self.chat_history}")
+        full_message = ""
+        for token in self.llm_client.chat_completion(
+            self.chat_history, max_tokens=100, stream=True
+        ):
+            # Some magic used by the Gradio library to stream response to GUI
+            full_message += token.choices[0].delta.content
+            yield full_message
+
+        return full_message
+
+    def generate_response(self, inference_client: InferenceClient, prompt: dict):
         """Function used for calling huggingface with streaming disabled"""
         response = inference_client.chat_completion(
             json={
@@ -38,14 +63,14 @@ class Librarian():
         )
         return json.loads(response.decode())[0]["generated_text"]
 
-
     def load_books(self, path):
         """Used to recursively load all books found in the library"""
-        loader = SimpleDirectoryReader(
-            input_dir=path,
-            recursive=True,
-            required_exts=[".epub"],
-        )
+        # loader = SimpleDirectoryReader(
+        #     input_dir=path,
+        #     recursive=True,
+        #     required_exts=[".epub"],
+        # )
 
-        documents = loader.load_data()
+        # documents = loader.load_data()
+        documents = []
         return documents
--- run.py
+++ run.py
@@ -1,38 +1,67 @@
 import gradio as gr
 import argparse
+import logging
 from agent.librarian import Librarian
-
-# Arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("--name", type=str, default="User")
-args, unknown = parser.parse_known_args()
-
-agent = Librarian("./books")
-# Gradio
-def predict(user_input, _):
-    global chat_history
-
-    # If it's the start of the conversation, include the system prompt
-    if chat_history is None or len(chat_history) == 0:
-        chat_history = [{"role": "system", "content": agent.system_prompt}]
-    else:
-        chat_history = chat_history.copy()  # Make a copy to avoid modifying the input directly
-
-    # Append the user message
-    chat_history.append({"role": "user", "content": user_input})
-
-    # Display librarian's answer
-    full_message = ""
-    for token in agent.llm_client.chat_completion(chat_history, max_tokens=100, stream=True):
-        full_message += token.choices[0].delta.content
-
-        yield full_message
-
-    # Append the assistant's response to the chat history
-    chat_history.append({"role": "assistant", "content": full_message})
+from logging.handlers import RotatingFileHandler
+from gradio.blocks import Blocks
 
 
+class GradioGUI(Blocks):
+    def __init__(self, name, lib_path, model_id):
+        """
+        Initializes the GUI, by loading the Librarian LLM.
+        """
+        self.agent = Librarian(name=name, lib_path=lib_path, model_id=model_id)
+        self.logger = logging.getLogger("librarian_logger")
 
-demo = gr.ChatInterface(predict) 
-demo.queue().launch()
+    def predict(self, user_input, _):
+        """
+        Wrapper around the GUI, called each time a new message is triggered.
 
+        :param user_input: message inserted by the user.
+        """
+        chat_history = self.agent.chat_history
+        self.logger.info(chat_history)
+
+        # If it's the start of the conversation, include the system prompt
+        if chat_history is None or len(chat_history) == 0:
+            chat_history.append({"role": "system", "content": self.agent.system_prompt})
+
+        # Append the user message
+        chat_history.append({"role": "user", "content": user_input})
+
+        full_message = ""
+        for token in self.agent.stream_response():
+            yield token
+            full_message += token
+
+        # Append the assistant's response to the chat history
+        chat_history.append({"role": "assistant", "content": full_message})
+
+    def launch(self):
+        """
+        Launch the user interface.
+        """
+        demo = gr.ChatInterface(self.predict)
+        demo.queue().launch()
+
+if __name__ == "__main__":
+    # Arguments with sensible default values
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", type=str, default="Razvan")
+    parser.add_argument(
+        "--model_id", type=str, default="meta-llama/Meta-Llama-3-8B-Instruct"
+    )
+    parser.add_argument("--lib_path", type=str, default="./books")
+    args, unknown = parser.parse_known_args()
+
+    # Configure the logger to facilitate development
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s',
+        handlers=[logging.FileHandler("debug.log")],
+    )
+
+    # Serve the server
+    demo = GradioGUI(name=args.name, model_id=args.model_id, lib_path=args.lib_path)
+    demo.launch()
